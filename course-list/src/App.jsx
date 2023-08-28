729edd44647b3bb6f0753ab8f20bbd7202f0d630
Armita Koleini <104152352+ArmitaKoleini@users.noreply.github.com>
2023-08-28 14:09:15 +0330

    Add files via upload

---
+++ course-list/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import CourseList from "./Components/CourseList";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <CourseList />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
