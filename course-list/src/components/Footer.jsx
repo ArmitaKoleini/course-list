c3113cdcc4f848ad3c3f54d71381bddd5afc8da4
Armita Koleini <104152352+ArmitaKoleini@users.noreply.github.com>
2023-09-08 12:53:12 +0330

    Delete course-list directory

--- course-list/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <div className="footer">
-        You have successfully passed <strong>33%</strong> of your courses.
-    </div>
-  )
-}
-
-export default Footer
